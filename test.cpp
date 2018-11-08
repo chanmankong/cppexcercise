e5262bf59fa85a02064430cd9df878da0807e971
daniel <vkccchan@gmail.com>
2018-11-08 10:04:53 +0800

    new input

--- test.cpp
+++ test.cpp
@@ -1,8 +1,23 @@
 #include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
 int main()
 {
-	vector<int> a(0, 10);
+	vector<int> a(10);
+
+	for(int i = 0; i < 10; i ++)
+	{
+		int  h;
+		cin >> h;
+		a.push_back(h);
+	}
+
+	auto r = find(a.begin(), a.end(), 88);
+
+	cout << *r << endl;
+
 
 	return 0;
 }
