b786cd0d268d8fb4749f490305f64075363c6b30
Chan Mankong <vkccchan@gmail.com>
2018-12-25 11:55:42 +0800

    modified bl-4133.cpp

--- bl-4133.cpp
+++ bl-4133.cpp
@@ -2,65 +2,45 @@
 #include <vector>
 using namespace std;
 
-void check(int &left, int &right, int &top, int &bottom, int x, int y)
+struct Node
 {
-	if(x < left)
-		left = x;
-	if(x > right)
-		right = x;
-	if(y < top)
-		top = y;
-	if(y > bottom)
-		bottom = y;
-
-	return;
-}
-
-int garbage(vector<vector<int> > &board, int x, int y, int d)
-{
-	int total = 0;
-	int left = max(0, x - d);
-	int right = min(1024, x + d);
-	int top = max(0, y - d);
-	int bottom = min(1024, y + d);
-
-	for(int i = left; i <= right; i++)
-		for(int j = top; j <= bottom; j++)
-			total += board[i][j];
-
-	return total;
-}
+	int x, y;
+	int garbage;
+	Node(int _x, int _y, int _g):x(_x), y(_y), garbage(_g){}
+};
 
 int main()
 {
 	int d, n;
 	cin >> d >> n;
 
-	vector<vector<int> > block(1025, vector<int>(1025, 0));
-	int left = 1024, right = 0, top = 1024, bottom = 0;
+	vector<Node> vec;
 	for(int i = 0; i< n; i++)
 	{
 		int x, y, t;
 		cin >> x >> y >> t;
-		block[x][y] = t;
-		check(left, right, top, bottom, x, y);
+		vec.push_back(Node(x, y, t));
 	}
 
 	int cnt = 0, max_garbage = 0;
-	int t = 0;
-	for(int i = left; i <= right; i++)
-		for(int j = top; j <= bottom; j++)
+	for(int i = 0; i < 1025; i++)
+		for(int j = 0; j < 1025; j++)
 		{
-			t = garbage(block, i, j, d);
-			if(t > max_garbage)
+			int sum = 0;
+			for(auto iter = vec.begin(); iter != vec.end(); iter++)
 			{
-				max_garbage = t;
+				if(iter->x >= i - d && iter->x <= i + d && iter->y >= j - d && iter->y <= j + d)
+					sum += iter->garbage;
+
+			}
+
+			if(sum > max_garbage)
+			{
+				max_garbage = sum;
 				cnt = 1;
 			}
-			else if(t == max_garbage)
+			else if(sum == max_garbage)
 				cnt++;
-			else
-				continue;
 		}
 
 	cout << cnt << " " << max_garbage << endl;
